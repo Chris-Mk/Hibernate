679b679c504567be1727c54a9076df0df1418189
Chris Mkolongo <chrismks23@gmail.com>
2019-09-11 16:56:57 +0300

    code first

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,14 @@
+package app;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Application {
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("sales");
+
+        entityManagerFactory.close();
+    }
+}
