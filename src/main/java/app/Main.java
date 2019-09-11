1531b1d20e4e9f5e32cb11eef43ac820ad9403a8
Chris Mkolongo <chrismks23@gmail.com>
2019-09-11 18:56:10 +0300

    code first

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("hospital");
+
+        entityManagerFactory.close();
+    }
+}
---
+++ src/main/java/entities/Patient.java
@@ -0,0 +1,85 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "patients")
+public class Patient extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private LocalDate dateOfBirth;
+    private byte[] picture;
+    private boolean hasMedicalInsurance;
+
+    public Patient() {
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Column
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "date_of_birth")
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Column
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    @Column(name = "has_medical_insurance")
+    public boolean isHasMedicalInsurance() {
+        return hasMedicalInsurance;
+    }
+
+    public void setHasMedicalInsurance(boolean hasMedicalInsurance) {
+        this.hasMedicalInsurance = hasMedicalInsurance;
+    }
+}
